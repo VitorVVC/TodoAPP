9e17faafd19c6bca8d4f67a3c44efb4b375a18e4
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-07-06 12:57:42 -0300

    Add new method for capture variables

---
+++ constants/env.go
@@ -0,0 +1,17 @@
+package constants
+
+const (
+	API_PORT      = "api.port"
+	POSTGRES_HOST = "database.host"
+	POSTGRES_PORT = "database.port"
+	POSTGRES_USER = "database.user"
+	POSTGRES_PASS = "database.pass"
+	POSTGRES_NAME = "database.name"
+
+	DefaultAPIPort      = "8080"
+	DefaultPostgresHost = "localhost"
+	DefaultPostgresPort = "5432"
+	DefaultPostgresUser = "user"
+	DefaultPostgresPass = "pass"
+	DefaultPostgresName = "dbname"
+)
