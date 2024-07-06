1aed640d4c5b4cec9c7ec02db5ea6e210034921a
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-07-06 11:52:22 -0300

    Refactor method past, models -> controllers

--- services/controllers/entities.go
+++ services/controllers/entities.go
@@ -1,8 +1,10 @@
-package models
+package controllers
 
 type Todo struct {
 	ID          int64  `json:"id"`
 	Title       string `json:"title"`
 	Description string `json:"description"`
 	Done        bool   `json:"done"`
+	InProgress  bool   `json:"in_progress"`
+	Priority    bool   `json:"priority"`
 }
