e9915389a7d71f465c766777adc0860db584688a
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-07-26 00:05:26 -0300

    Update todo struct

--- models/todo.go
+++ models/todo.go
@@ -1,10 +1,10 @@
 package models
 
 type Todo struct {
-	ID          int64  `json:"id"`
-	Title       string `json:"title"`
-	Description string `json:"description"`
-	Done        bool   `json:"done"`
-	InProgress  bool   `json:"in_progress"`
-	Priority    bool   `json:"priority"`
+	ID          int64  `json:"id" query:"id"`
+	Title       string `json:"title" query:"title"`
+	Description string `json:"description" query:"description"`
+	Done        bool   `json:"done" query:"done"`
+	InProgress  bool   `json:"inProgress" query:"inProgress"`
+	Priority    bool   `json:"priority" query:"priority"`
 }
