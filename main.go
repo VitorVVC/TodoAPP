8211bf4f0e7891133b2238cd2af28f5d0bab6c29
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-07-26 19:20:58 -0300

    Add load env

--- main.go
+++ main.go
@@ -4,11 +4,19 @@ import (
 	"api-postgresql/configs"
 	"api-postgresql/handlers"
 	"fmt"
+	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+	"log"
 )
 
 func main() {
+
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatalf("Erro ao carregar o arquivo .env: %v", err)
+	}
+
 	cfg, err := configs.LoadConfig()
 	if err != nil {
 		panic(fmt.Sprintf("Erro ao carregar a configuração: %v", err))
