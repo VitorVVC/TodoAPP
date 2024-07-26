fb8f0e76a9ac263c827b821c29d13fb96b371d29
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-07-26 00:08:12 -0300

    Better treatment to consts

--- constants/env.go
+++ constants/env.go
@@ -1,23 +1,10 @@
 package constants
 
-import "os"
-
-func getEnv(key, defaultValue string) string {
-	if value, exists := os.LookupEnv(key); exists {
-		return value
-	}
-	return defaultValue
-}
-
-var (
-	ApiPort      = getEnv("API_PORT", "9000")
-	PostgresHost = getEnv("POSTGRES_HOST", "localhost")
-	PostgresPort = getEnv("POSTGRES_PORT", "5432")
-	PostgresUser = getEnv("POSTGRES_USER", "user")
-	PostgresPass = getEnv("POSTGRES_PASS", "pass")
-	PostgresName = getEnv("POSTGRES_NAME", "dbname")
-)
-
 const (
-	FutureConst = "Hello World!"
+	ApiPort      = "API_PORT"
+	PostgresHost = "POSTGRES_HOST"
+	PostgresPort = "POSTGRES_PORT"
+	PostgresUser = "POSTGRES_USER"
+	PostgresPass = "POSTGRES_PASS"
+	PostgresName = "POSTGRES_NAME"
 )
