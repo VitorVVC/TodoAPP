ed6e72987882f5c2ed8b7788c67ab79594cdde31
Vitor Vargas Cardoso <144281355+VitorVVC@users.noreply.github.com>
2024-08-05 17:56:45 -0300

    Add BaseModel struct on todo

--- models/todo.go
+++ models/todo.go
@@ -3,6 +3,7 @@ package models
 import "github.com/google/uuid"
 
 type Todo struct {
+	BaseModel
 	UUID        uuid.UUID `json:"uuid" query:"uuid"`
 	Title       string    `json:"title" query:"title" validate:"required,max=30"`
 	Description string    `json:"description" query:"description"`
